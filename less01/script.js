b4beb822e10e97af9a85e4e1c403650897a3baef
zoduaks <zoduaks@gmail.com>
2021-08-01 19:19:10 +0300

    ready

--- less01/script.js
+++ less01/script.js
@@ -1,43 +1,40 @@
-var employers = ['АртеМ', 'максим', 'Владимир', 'сергей', 'НикиТа', 'евГений', ' Дарья', ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'];
-var nameCourse = 'Базовый React';
-var command = [];
-for (var i = 0; i < employers.length; i++) {
+'use strict';
+const employers = ['АртеМ', 'максим', 'Владимир', 'сергей', 'НикиТа', 'евГений', ' Дарья', ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'];
+const nameCourse = 'Базовый React';
+const command = [];
+
+//удалили из массива пустые значения и двойной перебор
+for (let i = 0; i < employers.length; i++) {
 	if (employers[i].length > 0 && employers[i].trim() != '') {
+		employers[i] = employers[i].toLowerCase().trim();
+		employers[i] = employers[i][0].toUpperCase() + employers[i].slice(1);
 		command.push(employers[i]);
 	}
 }
-for (var i = 0; i < command.length; i++) {
-	command[i] = command[i].toLowerCase().trim();
-	command[i] = command[i][0].toUpperCase() + command[i].slice(1);
-}
 
-var data = {
+const data = {
 	cash: [3, 8, 3],
 	react: ['JSX', 'components', 'props', 'state', 'hooks'],
 	add: ['styled-components', 'firebase']
 };
 
-function calcCash(own) {
-	own = own || 0;
-	var everyCash = Array.prototype.slice.call(arguments);
-	var total = own;
-	for (var i = 0; i < everyCash[1].length; i++) {
-		total += +everyCash[1][i];
-	}
-	return total;
+//не уверен. после пуша посмотрю как надо было )
+const calcCash = ({cash}) => {
+	let total;
+	total = cash.reduce((sum, current) => sum + current, 0);
 }
 
-var lesson = calcCash(null, data.cash);
+const lesson = calcCash(data);
 
 function makeBusiness(director, teacher, allModule, gang, course) {
 	teacher = teacher || 'Максим';
-	var sumTech = data.react.concat(data.add, 'и другие');
-	console.log('Стартуем новый курс: "' + course + '". Владелец: ' + director + ', преподаватель: ' + teacher + '. Всего уроков: ' + allModule + '. \nКоманда Академии: ' +
-		gang);
-	console.log('Первое что изучим будет ' + data.react[0] + ". Он очень похож на HTML!");
+	let sumTech = data.react.concat(data.add, 'и другие');
+	console.log(`Стартуем новый курс: "${course}". Владелец: ${director}, преподаватель: ${teacher}. Всего уроков: ${allModule}.
+	Команда Академии: ${gang}`);
+	console.log(`Первое что изучим будет ${data.react[0]}. Он очень похож на HTML!`);
 	console.log('Технологии которые мы изучим: ');
-	console.log.apply(null, sumTech);
+	console.log(...sumTech);
 
 }
 
-makeBusiness.apply(null, ['Артем', null, lesson, command, nameCourse]);
+makeBusiness(...['Артем', null, lesson, command, nameCourse]);
